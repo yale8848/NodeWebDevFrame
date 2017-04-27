fc13fc6ac3932b96f956428a91cc8d7265c3401e
yale8848 <royal8848@163.com>
2017-04-27 16:45:05 +0800

    modify

--- dao/BaseDao.js
+++ dao/BaseDao.js
@@ -1,23 +1,66 @@
+let log = require('../utils/Log');
 module.exports = class UserDao {
     constructor(dao) {
         this.dao = dao;
 
     }
     find(option) {
-        return this.dao.findAll(option);
+        return new Promise((res, rej) => {
+            this.dao.findAll(option).then(s => {
+                res(s);
+            }, e => {
+                log.error("" + e);
+                rej(e);
+            });
+        });
     };
     add(data) {
-        return this.dao.create(data);
+
+        return new Promise((res, rej) => {
+            this.dao.create(data).then(s => {
+                res(s);
+            }, e => {
+                log.error("" + e);
+                rej(e);
+            });
+        });
+
     }
     deleteWhere(whr) {
-        return this.dao.destroy({
-            where: whr
+
+        return new Promise((res, rej) => {
+            this.dao.destroy({
+                where: whr
+            }).then(s => {
+                res(s);
+            }, e => {
+                log.error("" + e);
+                rej(e);
+            });
         });
+
     }
     findWhere(whr) {
-        return this.dao.findOne({ where: whr });
+        return new Promise((res, rej) => {
+            this.dao.findOne({ where: whr }).then(s => {
+                res(s);
+            }, e => {
+                log.error("" + e);
+                rej(e);
+            });
+        });
+
+
     }
     updateWhere(data, whr) {
-        return this.dao.update(data, { where: whr });
+
+        return new Promise((res, rej) => {
+            this.dao.update(data, { where: whr }).then(s => {
+                res(s);
+            }, e => {
+                log.error("" + e);
+                rej(e);
+            });
+        });
     }
 }
